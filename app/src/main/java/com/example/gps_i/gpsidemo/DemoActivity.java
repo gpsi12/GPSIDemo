22f5551cc2cf4024ce073476042f0643dcda5468
gps-i <gps-i@foxmail.com>
2019-04-10 21:39:46 +0800

    feat(project): 解决了跳转DemoeActivity失败

--- app/src/main/java/com/example/gps_i/gpsidemo/DemoActivity.java
+++ app/src/main/java/com/example/gps_i/gpsidemo/DemoActivity.java
@@ -4,11 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 
-public class DemoActivity extends MainActivity {
+public class DemoActivity extends Activity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState,  PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
     }
+
 }
