7911dfade85664b2950a8e83e627a21b198b1339
gpsi <gps-i@foxmail.com>
2020-03-24 22:53:30 +0800

    feat(common):创建存放项目公用工具类、Base类、库

--- feature_chat/src/main/java/com/example/feature_chat/ChatActivity.java
+++ feature_chat/src/main/java/com/example/feature_chat/ChatActivity.java
@@ -1,7 +1,6 @@
 package com.example.feature_chat;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
